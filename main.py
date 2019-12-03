770e7e472d8f123d7df9b19a26243b27abb979fe
Jonathan Obenland <jobenland1@gmail.com>
2019-12-02 23:56:00 -0500

    Added working Highscore function

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
 import time
+import json
 
 
-def main():
+
+def welcome():
+
     print("Welcome to Hangman")
-    print("---Menu---")
-    print("1---Start Game")
-    print("2---See High Scores")
-    choice = input("Choose an option")
-    if choice == 1:
-        gameStart()
-    if choice == 2:
-        highScores()
+    print("--------------")
+    print("|    Menu    |")
+    print("--------------\n")
+    print("1 >>>> Start Game")
+    print("2 >>>> High Scores\n")
+    menu()
+
+def menu():
+
+    while True:
+        choice = input("Choose an option: ")
+        if choice == "1":
+            gameStart()
+            break
+        if choice == "2":
+            highScores()
+            break
+        else:
+            print(choice, "was not a valid option! try agian...")
 
 def gameStart():
     #Andrew
@@ -19,8 +33,30 @@ def gameStart():
     #   time
     #   num Incorrect
     print("code here")
+    score={}
+    time = 57
+    attempts = 77
+    scoreName = input("Congrats!, Enter your name: ")
+    score[scoreName] = {'time' : time, 'attempts' : attempts}
+    highScores(score)
 
-def highScores():
+def highScores(score):
     #Jonathan
-    print("code here")
+    try:
+        with open ("highscores.json") as f:
+            data = json.load(f)
 
+        data.update(score)
+
+        with open ("highscores.json","w") as f:
+            json.dump(data, f)
+
+    except FileNotFoundError:
+        print("Since this is your first time playing, a high score file has been created for you")
+
+        with open ("highscores.json",'w') as f:
+            json.dump(score, f)
+
+    print("file")
+
+welcome()
