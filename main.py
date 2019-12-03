150287243a17c6ac8586318b23558c068ce6e3c6
Jonathan Obenland <49774425+Jobenland@users.noreply.github.com>
2019-12-02 22:30:41 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+import time
+
+
+def main():
+    print("Welcome to Hangman")
+    print("---Menu---")
+    print("1---Start Game")
+    print("2---See High Scores")
+    choice = input("Choose an option")
+    if choice == 1:
+        gameStart()
+    if choice == 2:
+        highScores()
+
+def gameStart():
+    #Andrew
+    #needs to store...
+    #   attempts
+    #   time
+    #   num Incorrect
+    print("code here")
+
+def highScores():
+    #Jonathan
+    print("code here")
+
